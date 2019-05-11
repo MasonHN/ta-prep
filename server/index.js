f53ee5000c0efe838cb180031db72e7b619f8e7b
MasonHN <hunternmason@gmail.com>
2019-05-11 13:30:43 -0500

    starting

--- server/index.js
+++ server/index.js
@@ -10,9 +10,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", express.static("dist"));
 
-app.get("/api", (req, res) => {
-  console.log("successful request!");
-  res.send("Hi there");
+app.get("/todos", (req, res) => {
+  request.get('https://jsonplaceholder.typicode.com/todos', (err, response, body) => {
+    if (err) {
+      console.log('error', err)
+    } else {
+      console.log('body', typeof(body));
+      res.send(JSON.parse(body));
+    }
+  })
+  // console.log("successful request!", req.body);
+  // res.send("Hi there");
 });
 
 app.listen(3000, () => console.log("Now listening on port 3000!"));
